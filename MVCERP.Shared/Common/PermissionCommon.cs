45e09056da80cd396ecf758331a5ca558a577f5d
Navraj Bhattarai <www.navarajbhattarai11@gmail.com>
2021-02-21 11:54:11 +0545

    # modified permission index

--- MVCERP.Shared/Common/PermissionCommon.cs
+++ MVCERP.Shared/Common/PermissionCommon.cs
@@ -5,16 +5,25 @@ using System.Text;
 
 namespace MVCERP.Shared.Common
 {
-    public class PermissionCommon :RoleCommon
+    public class PermissionCommon
     {
         public string ParentMenu { get; set; }
         public string Menu { get; set; }
         public int MenuId { get; set; }
-        public bool MenuCheck { get; set; }
         public string URL { get; set; }
         public string UserName { get; set; }
         public string FK_ID { get; set; }
         public string FK_RoleId { get; set; }
         public string FK_MenuId { get; set; }
+        public string RoleName { get; set; }
+        public string Id { get; set; }
+        public string IsActive { get; set; }
+        public string RolePermission { get; set; }  
+    }    
+    public class RolePermission 
+    {
+        public string Id { get; set; }
+        public string RoleID { get; set; }
     }
+
 }
